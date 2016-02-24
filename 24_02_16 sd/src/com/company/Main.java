3c10bb035eb7e82f32e2cdc519c16b4a2a3ce3c9
shalomd <hackeru@C4S27.Hackeru2.local>
2016-02-24 15:59:04 +0200

    24_02_16

--- 24_02_16 sd/src/com/company/Main.java
+++ 24_02_16 sd/src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("sqrt 15 = "+ sqrt(90));
+    }
+    public static double sqrt(double x){
+        double root = 0;
+        double s = 1.0;
+        double nextResult;
+            while ((root *root) <x ) {
+                nextResult = root+s;
+                if ((nextResult  * nextResult ) > x) {
+                    s /= 10;
+                    if (s < 0.00001)
+                        break;
+                }
+                root += s;
+            }
+        return root;
     }
 }
